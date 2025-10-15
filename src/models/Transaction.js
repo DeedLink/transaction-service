4ed42af6cac22d28c12e549f6b9908e89796a7ba
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-10-15 07:20:29 +0530

    update

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -7,7 +7,7 @@ const transactionSchema = new mongoose.Schema({
   status: { type: String, enum: ["pending", "completed", "failed"], default: "pending" },
   hash: { type: String, required: true },
   amount: { type: Number, required: true },
-  type: { type: String, enum: ["gift", "open_market", "direct_transfer", "closed", "init"], required: true },
+  type: { type: String, enum: ["gift", "open_market", "direct_transfer", "closed", "init", "sale_transfer", "escrow_sale"], required: true },
   date: { type: Date, default: Date.now },
   description: { type: String },
   share: { type: Number, default: 100 },
