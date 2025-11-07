b86dc83c9a2a98e00449486f4b0d924a8c39dad0
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-11-07 20:30:54 +0530

    event type added to mq

--- src/mQ/transactionProducer.js
+++ src/mQ/transactionProducer.js
@@ -6,17 +6,29 @@ dotenv.config();
 const RABBITMQ_URL = process.env.RABBITMQ_URL;
 const QUEUE = "transaction_events";
 
-export const publishTransaction = async (transaction) => {
+/**
+ * Publish a transaction event
+ * @param {Object} transaction - transaction data
+ * @param {string} eventType - type of the event, e.g:"transaction_created"
+ */
+export const publishTransaction = async (transaction, eventType) => {
   try {
     const connection = await amqp.connect(RABBITMQ_URL);
     const channel = await connection.createChannel();
 
     await channel.assertQueue(QUEUE, { durable: true });
-    channel.sendToQueue(QUEUE, Buffer.from(JSON.stringify(transaction)), {
+
+    const message = {
+      eventType,
+      transaction,
+      timestamp: new Date(),
+    };
+
+    channel.sendToQueue(QUEUE, Buffer.from(JSON.stringify(message)), {
       persistent: true,
     });
 
-    console.log("Transaction event sent:", transaction);
+    console.log("Transaction event sent:", message);
 
     await channel.close();
     await connection.close();
