3d7fb1b25b54e4676bce79f1cbc680d940f66100
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-11-14 20:55:16 +0530

    update then order of the routes

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,20 +1,29 @@
 import express from "express";
-import { createTransaction, deleteTransaction, getAllTransactions, getTransactionById, getTransactionsByDeed, getTransactionsByStatus, getTransactionsByUser, updateStatus, updateTransaction } from "../controllers/transactionController.js";
+import { 
+  createTransaction, 
+  deleteTransaction, 
+  getAllTransactions, 
+  getTransactionById, 
+  getTransactionsByDeed, 
+  getTransactionsByStatus, 
+  getTransactionsByUser, 
+  updateStatus, 
+  updateTransaction 
+} from "../controllers/transactionController.js";
 
 const router = express.Router();
 
 router.post("/", createTransaction);
 router.get("/", getAllTransactions);
-router.get("/:id", getTransactionById);
-router.put("/:id", updateTransaction);
-router.delete("/:id", deleteTransaction);
 
-// Additional filters
 router.get("/status/:status", getTransactionsByStatus);
 router.get("/user/:address", getTransactionsByUser);
 router.get("/deed/:deedId", getTransactionsByDeed);
 
-// Partial Updates
 router.post("/status/:blockchain_identification", updateStatus);
+
+router.get("/:id", getTransactionById);
+router.put("/:id", updateTransaction);
+router.delete("/:id", deleteTransaction);
 
 export default router;
