0597a28bdc34bd6b5bdd7e502e863dfb313dfb5e
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-11-16 12:20:59 +0530

    updated

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,16 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
+
+    const db = mongoose.connection;
+    const transactionsCollection = db.collection("transactions");
+
+    return transactionsCollection;
   } catch (error) {
     console.error("MongoDB connection failed:", error);
     process.exit(1);
